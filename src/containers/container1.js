628755e4e6bdfc11587f4282150e5523988b6731
Sushil <developer.timilsina@gmail.com+>
2019-11-20 22:57:56 +0545

    states

--- src/containers/container1.js
+++ src/containers/container1.js
@@ -1,12 +1,47 @@
 import  React, { Component} from 'react'
+import  Component1 from '../functional/component1'
 
 class Container1 extends  Component{
+
+    constructor (props) {
+        super(props)
+
+        this.state= {
+            stateprop1: "Our initial state",
+            stateprop2: 5
+        }
+    }
+
+    //Wrong way
+    // changestate = () => (
+    //     this.state.stateprop1 = "New State"
+    // )
+
+   // Correct way
+    changeState = () => (
+        this.setState((prevState, props) =>({
+            stateprop2: prevState.stateprop2 +1
+        })
+    ))
+
+    changeState2 = () => (
+        this.setState({
+            stateprop1: this.state.stateprop1 + "L"
+        })
+    )
+
 
     render() {
         return (
             <div>
-                {this.props.nickname}
-                {this.props.height}
+                <button onClick={() => this.changeState()}>Change State</button>
+                <button onClick={() => this.changeState2()}>Change State</button>
+                <br/>
+                <Component1 prop1={this.state.stateprop1}/>
+                <br/>
+                {this.state.stateprop2}
+                <br/>
+                {this.state.stateprop1}
             </div>
         )
     }
--- src/functional/component1.js
+++ src/functional/component1.js
@@ -4,6 +4,7 @@ const Component1 = props =>(
     <div>
         {props.name}
         {props.age}
+        {props.prop1}
     </div>
 );
 
