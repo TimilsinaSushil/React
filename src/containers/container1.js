dbb232dd018f9a248e4cfee4fe3be3c8e9cdd5a0
Sushil <developer.timilsina@gmail.com+>
2019-11-25 22:16:02 +0545

    Worjing ith arrays/ Mapping array

--- src/containers/container1.js
+++ src/containers/container1.js
@@ -1,47 +1,28 @@
-import  React, { Component} from 'react'
-import  Component1 from '../functional/component1'
+import React, {Component} from 'react'
+import Component1 from '../functional/component1'
 
-class Container1 extends  Component{
+class Container1 extends Component {
 
-    constructor (props) {
-        super(props)
+    Arr1 = [
+        {id:1, text:'text1', number:1},
+        {id:2, text:'text2', number:2},
+        {id:3, text:'text3',number:3},
+        {id:4, text:'text4',number:4},
+        {id:5, text:'text5',number:5},
+    ]
 
-        this.state= {
-            stateprop1: "Our initial state",
-            stateprop2: 5
-        }
-    }
+    RenderListItem = (props) => (
+        <div>
+            {props.item.text}
+            <p>{props.item.number}</p>
 
-    //Wrong way
-    // changestate = () => (
-    //     this.state.stateprop1 = "New State"
-    // )
-
-   // Correct way
-    changeState = () => (
-        this.setState((prevState, props) =>({
-            stateprop2: prevState.stateprop2 +1
-        })
-    ))
-
-    changeState2 = () => (
-        this.setState({
-            stateprop1: this.state.stateprop1 + "L"
-        })
+        </div>
     )
-
 
     render() {
         return (
             <div>
-                <button onClick={() => this.changeState()}>Change State</button>
-                <button onClick={() => this.changeState2()}>Change State</button>
-                <br/>
-                <Component1 prop1={this.state.stateprop1}/>
-                <br/>
-                {this.state.stateprop2}
-                <br/>
-                {this.state.stateprop1}
+                {this.Arr1.map((item, index) => <this.RenderListItem key={item.id} item={item}/>)}
             </div>
         )
     }
